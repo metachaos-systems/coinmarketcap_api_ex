96d503bf99f3859c94fd9895ed232c15c021879d
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-01-04 10:47:07 +0200

    fetch known coins data test

--- test/coinmarketcap_com_test.exs
+++ test/coinmarketcap_com_test.exs
@@ -5,6 +5,11 @@ defmodule CoinmarketcapApiTest do
 
   test "fetch coin data for bitcoin" do
     {:ok, data} = fetch_coin_data("bitcoin")
-    assert  %{"market_cap_by_available_supply" => [[_, _] | _ ]}  = data
+    assert %{"market_cap_by_available_supply" => [[_, _] | _ ]}  = data
+  end
+
+  test "fetch known coins data" do
+    {:ok, data} = fetch_known_coins_data()
+    assert [%{"name" => _, "slug" => _} | _ ] = data
   end
 end
