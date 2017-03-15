e329ca05311f4598a9195489b76bfa6fa3a032ce
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-03-15 12:00:43 +0200

    update matching an API url change

--- lib/coinmarketcap_com.ex
+++ lib/coinmarketcap_com.ex
@@ -1,14 +1,14 @@
 defmodule CoinmarketcapApi do
   use HTTPoison.Base
   @http_opts [timeout: 15_000]
-  
+
   @doc """
   Intended for coinmarketcap.com for showing historical data
   Returns map with the following keys: marketcap_by_available_supply, prices_btc, prices_usd, volume_usd
   Data is in the format of [time, value] where time is measured in ticks(milliseconds since the start of unix era)
   """
   def fetch_coin_data(name) do
-    "https://api.coinmarketcap.com/v1/datapoints/" <> name <> "/" # slash is canonical path, URL without slash redirects
+    "https://graphs.coinmarketcap.com/v1/datapoints/" <> name <> "/" # slash is canonical path, URL without slash redirects
       |> get([],@http_opts)
       |> process_result()
   end
