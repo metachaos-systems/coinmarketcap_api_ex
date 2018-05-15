41df6892ec05b672209f67fce0c2197cf6a1f356
ontofractal <ontofractal@protonmail.com>
2018-05-15 17:57:27 +0100

    add parsing for global data

--- lib/coinmarketcap_api/global_data.ex
+++ lib/coinmarketcap_api/global_data.ex
@@ -8,4 +8,13 @@ defmodule CoinmarketcapApi.GlobalData do
   ]
 
   use ExConstructor
+
+  def parse(data) do
+    data
+    |> Map.update!(:last_updated, &parse_times/1)
+  end
+
+  def parse_times(unix_time) do
+    DateTime.from_unix!(unix_time) |> DateTime.to_naive()
+  end
 end
