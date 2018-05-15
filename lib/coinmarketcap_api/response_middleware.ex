59166f746d7d095cb7b97c4ac1c22ac1e198b536
ontofractal <ontofractal@protonmail.com>
2018-05-15 17:06:09 +0100

    add Ticker structs construction for matching endpoints

--- lib/coinmarketcap_api/response_middleware.ex
+++ lib/coinmarketcap_api/response_middleware.ex
@@ -1,17 +1,47 @@
 defmodule CoinmarketcapApi.ResponseMiddleware do
   @behaviour Tesla.Middleware
+  alias CoinmarketcapApi.Ticker
 
-  def call(env, next, options) do
+  def call(env, next, _options) do
     env
     |> Tesla.run(next)
     |> handle_response()
   end
 
   def handle_response(response) do
-    with {:ok, %{body: %{"data" => data, "metadata" => meta}}} <- response do
+    # {:ok, env_test} = response
+    # IO.inspect Map.keys(env_test)
+    with {:ok, env = %{body: %{"data" => data, "metadata" => meta}, url: url}} <- response do
+      data =
+        if is_ticker_endpoint?(url) do
+          construct_tickers(data)
+        else
+          data
+        end
+
       {:ok, %{data: data, metadata: meta}}
     else
       err -> err
     end
+  end
+
+  def is_ticker_endpoint?(url) do
+    String.contains?(url, "/v2/ticker/")
+  end
+
+  defp construct_tickers(data) do
+    if is_nil(data["id"]) do
+      for {k, v} <- data, into: %{} do
+        {k, construct_tickers(v)}
+      end
+    else
+      construct_ticker(data)
+    end
+  end
+
+  defp construct_ticker(datum) do
+    datum
+    |> Ticker.new()
+    |> Ticker.parse()
   end
 end
